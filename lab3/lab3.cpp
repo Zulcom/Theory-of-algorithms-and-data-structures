c9a91094ca83dd33b31a59b099396785818bbd07
Zulcom <s01@ngs.ru>
2016-10-31 22:39:39 +0700

    Final version to defence. Update gititnore.

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -1,107 +1,76 @@
 ﻿#include <iostream>
 #include <conio.h>
-#include <ctime>
-#include <clocale>
 #include <string>
 #include <fstream>
 #include <algorithm>
 
 using namespace std;
 
-
-string find(string *array, int n, string myStr)
-{
-	if(myStr == array[0]) return array[1];
-	if(array[n - 1] == myStr) return array[n - 2];
-
+string findLin(string *inputLines, int n, string search){
+	if(inputLines[0] == search) return inputLines[1];
+	if(inputLines[n - 1] == search) return inputLines[n - 2];
 	for(int i = 0; i < n; ++i)
-	{
-		if(myStr == array[i])
-		{
-			return array[i - 1];
-		}
-	}
-	return "no";
+		if(search == inputLines[i])
+			return inputLines[i - 1];
+	return "-1";
 }
-
-string findBin(string *array, int n, string myStr)
+string findBin(string *inputLines, int n, string search)
 {
-	if(myStr == array[0]) return array[1];
-	if(array[n - 1] == myStr) return array[n - 2];
-
+	if(inputLines[0] == search ) return inputLines[1];
+	if(inputLines[n - 1] == search) return inputLines[n - 2];
 	string midStr;
 	int mid, left = 0,
 		right = n;
-	while((right - left) > 1)
-	{
+	while(right - left > 1){
 		mid = (right + left) / 2;
-		midStr = array[mid];
-		if(midStr == myStr)
-		{
-			return array[mid - 1];
-		}
-		else if(midStr.length() == myStr.length())
-		{
-			for(int i = 0; 0 < myStr.length();)
-			{
-				if(midStr[i] == myStr[i]) ++i;
-				else if(midStr[i] > myStr[i]) { right = mid; break; }
-				else { left = mid; break; }
-			}
-		}
+		midStr = inputLines[mid];
+		if(midStr == search) return inputLines[mid - 1];
+		if(midStr.length() == search.length())
+			for(int i = 0; 0 < search.length();)
+				if(midStr[i] == search[i]) ++i;
+				else{
+					if(midStr[i] > search[i]) right = mid;
+					else left = mid;
+					break;
+				}
 		else
-		{
-			if(midStr.length() > myStr.length()) right = mid;
+			if(midStr.length() > search.length()) right = mid;
 			else left = mid;
-		}
 	}
-	return "no";
+	return "-1";
 }
-
-
-
+string(*searches[2])(string*, int, string) = {findLin,findBin};
 int main()
 {
-	setlocale(LC_ALL, "russian");
-
-	ifstream file("test.txt");
-	if(!file.is_open())
+	ifstream input("test.txt");
+	if(!input.is_open())
 	{
-		cout << "Невозможно открыть файл" << endl;
+		cout << "Error handled on file open" << endl;
 		system("pause");
 		return 0;
 	}
-
 	int n;
-	file >> n;
-
-	string *arrStr = new string[n];
-	for(int i = 0; i < n; ++i)
+	input >> n;
+	string *inputLines = new string[n];
+	for(int i = 0; i < n; ++i) input >> inputLines[i];
+	cout << "Lines in file: " << endl;
+	for(int i = 0; i < n; ++i) cout << inputLines[i] << endl;
+	string search, result;
+	cout << "Input what are you serching for: ";
+	cin >> search;
+	cout << "Inport number of search alorithm:\n1.Linear Search\n2.Binary Search\n";
+	int choise;
+	cin >> choise;
+	while(choise >2 || choise<1)
 	{
-		file >> arrStr[i];
+		cout << "Incorrect number of alogithm, please try again" << endl;
+		cout << "Inport number of search alorithm:\n1.Linear Search\nBinary Search\n";
+		cin >> choise;
 	}
-
-	cout << "Данные из файла: " << endl;
-	for(int i = 0; i < n; ++i)
-	{
-		cout << arrStr[i] << endl;
-	}
-	string myStr, findStrBin, findStrInter, findStr;
-	cout << "Введите искомую строку: ";
-	cin >> myStr;
-
-	cout << "\nНаиболее близкий к искомому элемент " << endl;
-
-	findStr = find(arrStr, n, myStr);
-	if(findStr == "no")
-		cout << "Данного элемента не существует." << endl << endl;
-	cout << "Последовательный поиск: " << findStr << endl << endl;
-
-	findStrBin = findBin(arrStr, n, myStr);
-	if(findStrBin == "no")
-		cout << "Данного элемента не существует." << endl << endl;
-	cout << "Двоичный поиск: " << findStrBin << endl << endl;
-
+	
+	result = searches[choise-1](inputLines, n, search);
+	if(result == "-1") cout << "Line not found in file" << endl;
+	else cout << "\nClosest line: "<< result << endl;
 	system("pause");
 	return 0;
 }
